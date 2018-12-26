bfe0d45f0f012e3b4f6c53a345b03e7bbef8a671
Servio Zambrano <servio.za@gmail.com>
2018-12-26 14:17:28 -0300

    test suggestion

--- app/Repository/Domain.php
+++ app/Repository/Domain.php
@@ -8,8 +8,11 @@ class Domain
     public function getPrice($agent)
     {
 
-        $parameters = (object) $agent->getParameters();
+        $parameters =  $agent->getParameters();
 
+        $suggestion = \Dialogflow\RichMessage\Suggestion::create(['Suggestion one', 'Suggestion two']);
+
+        return $suggestion;
 
         return "Hola el dominio $parameters->domain tiene un costo de 10$(USD)";
 
