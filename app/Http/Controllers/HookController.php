83d036e61df062011232f294d12772ec7f03a76a
Servio Zambrano <servio.za@gmail.com>
2018-12-26 13:10:49 -0300

    except verifycsrf to point

--- app/Http/Controllers/HookController.php
+++ app/Http/Controllers/HookController.php
@@ -7,8 +7,11 @@ use Dialogflow\WebhookClient;
 
 class HookController extends Controller
 {
+
+
     public function point(Request $request)
     {
+        return "epa";
         $agent = WebhookClient::fromData($request->json()->all());
 
         $agent->reply('Hi, how can I help?');
