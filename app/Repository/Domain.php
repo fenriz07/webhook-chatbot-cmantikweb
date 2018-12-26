fdcc5c715b1c082a8e492a9145f2c16b1edf7e22
Servio Zambrano <servio.za@gmail.com>
2018-12-26 15:54:29 -0300

    quiero un dominio

--- app/Repository/Domain.php
+++ app/Repository/Domain.php
@@ -27,11 +27,11 @@ class Domain
 
         $cliente =  (object) $agent->getParameters();
 
-        Mail::send('emails.buyDomain',['cliente' => $cliente],function($m) use ($cliente){
+ /*        Mail::send('emails.buyDomain',['cliente' => $cliente],function($m) use ($cliente){
             $m->to($cliente->correo,$cliente->nombre);
             $m->from('servio@cmantikweb.com','Cmantikweb-BOT');
             $m->subject('Instrucciones para la compra de un dominio.');
-        });
+        }); */
  
         return "Listo $cliente->nombre, te he enviado un correo al $cliente->correo con las instrucciones para finalizar la compra.";
     }
