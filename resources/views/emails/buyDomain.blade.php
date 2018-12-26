218dd0d211a48ffe112093d551a0efd31dba72bd
Servio Zambrano <servio.za@gmail.com>
2018-12-26 17:26:53 -0300

    push

--- resources/views/emails/buyDomain.blade.php
+++ resources/views/emails/buyDomain.blade.php
@@ -1,4 +1,4 @@
-Hola {{ $cliente->nombre}} {{ $cliente->apellido }} <br>
+Hola {{ $cliente['nombre']}} {{ $cliente['apellido'] }} <br>
 
 Se ha reservado  bajo la reserva <strong>#78155</strong> <br>
 
