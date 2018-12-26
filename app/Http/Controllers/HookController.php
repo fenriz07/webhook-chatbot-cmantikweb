d6923e1a5e4c5f71c4bae360040c0ab22841d674
Servio Zambrano <servio.za@gmail.com>
2018-12-26 12:58:23 -0300

    Hook Controller add

---
+++ app/Http/Controllers/HookController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Dialogflow\WebhookClient;
+
+class HookController extends Controller
+{
+    public function point(Request $request)
+    {
+        $agent = WebhookClient::fromData($request->json()->all());
+
+        $agent->reply('Hi, how can I help?');
+
+        return response()->json($agent->render());
+        //return dd($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::post('/point','HookController@point');
